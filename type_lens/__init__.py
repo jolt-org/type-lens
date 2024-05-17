a5b8157fb6045f2a06ebc17fa9b5a4c16c03f7b7
DanCardin <ddcardin@gmail.com>
2024-05-16 20:07:51 -0400

    feat: Implement a FunctionView.

--- type_lens/__init__.py
+++ type_lens/__init__.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+from .function_view import FunctionView
 from .parameter_view import ParameterView
 from .type_view import TypeView
 
 __all__ = (
+    "FunctionView",
+    "FunctionView",
     "ParameterView",
     "TypeView",
 )
